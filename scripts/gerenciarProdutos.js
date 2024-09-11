2ad9db479ee834c95623f4dc80f2cf712e280eae
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-09-11 04:08:39 +0000

    testes

---
+++ scripts/gerenciarProdutos.js
@@ -0,0 +1,44 @@
+class GerenciadorProdutos{
+    static produtos = [];
+
+    static adicionarProduto(produto){
+        this.produtos.push(produto)
+        console.log('Produto adicionado:', produto.nome );
+
+    }
+
+    static removerProdutos(produto){
+        this.produtos = this.produtos.filter(u => u.email !== usuario.email);
+        console.log('Produto excluido: ${usuario.nome}');
+    }
+
+    static listarProdutos(){
+        if(this.produtos.length === 0){
+            console.log('Nenhum produto cadastrado.');
+        } else {
+            console.log('Lista de produtos cadastrados');
+
+            this.produtos.forEach(
+                usuario => {
+                    console.log(produto.toString());
+                }
+            );
+        }
+    }
+    
+    static encontrarProduto(nome){
+        const produto = this.produto.find(u => u.nome === nome);
+
+        if(produto){
+            console.log(produto.toString())
+            return produto;
+        } else {
+            console.log('Produto não encontrado');
+            return null;
+        }
+    }
+    static obterProdutos(){
+        return this.produtos;
+    }
+}
+module.exports = GerenciadorProdutos;
