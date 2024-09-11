f1e60b50ae514cbaf034cb220f1834036611b0b2
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-09-11 04:39:19 +0000

    sei lá

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 const port = 3000; 
 
 const Usuario = require('./scripts/usuario')
-const GerenciadorUsuarios = require('./scripts/gerenciadorUsuarios')
+const GerenciadorUsuarios = require('./scripts/gerenciadorUsuarios');
+const GerenciadorProdutos = require('./scripts/gerenciarProdutos');
 
 app.use(express.urlencoded({ extended: true}));
 
@@ -69,6 +70,49 @@ app.post('/cadastro', (req, res)=> {
         }
      
     });
+app.get('/cadastroproduto', (req, res)=> {
+    fs.readFile(path.join(__dirname, "/public/pages/cadastroproduto.html"), (err, data)=>{
+        if(err){
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Erro interno do servidor.');
+            return;
+        }
+    
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.end(data);
+    });    
+});
+app.post('/cadastroproduto', (req, res)=> {
+    const vendedor = req.body.vendedor;
+    const nomeproduto = req.body.nomeproduto;
+    const tipoproduto = req.body.tipoproduto;
+    const repetirSenha = req.body.pwdRpt;
+
+    if(senha !== repetirSenha){
+        res.send(` <script>
+            alert("As senhas digitadas são diferentes...");
+            location.href = "/cadastro"
+        </script> `);        
+    } else {
+        const usuario = new Usuario(nome, email, senha);
+        GerenciadorUsuarios.adicionarUsuario(usuario);
+
+        res.redirect('/');
+    }
+
+
+    const usuario1 = new Usuario('a, luciano@gmail.com, a');
+    
+    GerenciadorUsuarios.adicionarUsuario(usuario1);
+
+        if(usuarioEncontrado){
+            console.log('Encontrado: ', usuarioEncontrado.toString('alexandre@gmail.com'));
+        }else{
+            console.log('Usuário não encontrado')
+        }
+     
+});
+
 
 app.get('/salgados', (req, res) =>{
     fs.readFile(path.join(__dirname, "public/pages/salgados.html"), (err, data)=>{
@@ -162,6 +206,18 @@ app.get('/mostrar-usuarios', (req, res)=>{
     res.send(html);
 
 })
+app.get('/mostrar-produtos', (req, res)=>{
+    const produtos = GerenciadorProdutos.obterProdutos();
+    let html = '<h1> Lista de Produtos <h1><ul>';
+
+    produtos.forEach(produto => {
+        html += `<li> Nome do vendedor: ${produto.vendedor}, Preço: ${produto.preco} </li>`
+    })
+    html += '</ul>'
+    res.send(html);
+
+})
+
 
 app.listen(port, ()=>{
     console.log('Servidor rodando em http://localhost:'+ port);
